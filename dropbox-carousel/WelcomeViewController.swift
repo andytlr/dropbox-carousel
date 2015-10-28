de5c32e0c5a387d11c1cd4055f623fd414ec2baf
Andy Taylor <andytlr@gmail.com>
2015-10-27 18:04:36 -0700

    Welcome screen basics

---
+++ dropbox-carousel/WelcomeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  WelcomeViewController.swift
+//  dropbox-carousel
+//
+//  Created by Andy Taylor on 10/27/15.
+//  Copyright © 2015 Andy Taylor. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        scrollView.contentSize = imageView.frame.size
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func tapSignInButton(sender: AnyObject) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
